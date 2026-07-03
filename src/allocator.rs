969b8c6b968009b5e0a87adefa15600d7cde9a0a
Jacob MacKenzie-Websdale <jacobmacweb@gmail.com>
2026-07-03 08:44:50 +0200

    Fixed block size allocator

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,5 +1,5 @@
-use alloc::alloc::{GlobalAlloc, Layout};
-use core::ptr::null_mut;
+pub mod block;
+pub mod lock;
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
@@ -39,7 +39,8 @@ pub fn init_heap(
     Ok(())
 }
 
-use linked_list_allocator::LockedHeap;
+use block::FixedSizeBlockAllocator;
 
 #[global_allocator]
-static ALLOCATOR: LockedHeap = LockedHeap::empty();
+static ALLOCATOR: lock::Locked<FixedSizeBlockAllocator> =
+    lock::Locked::new(FixedSizeBlockAllocator::new());
